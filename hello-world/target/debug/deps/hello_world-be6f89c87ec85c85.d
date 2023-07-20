45a002b1a6ee5cd1a3e3733b98db9cbb36d20bbf
pole9774 <pole97@live.it>
2023-07-20 16:00:01 +0200

    change

---
+++ hello-world/target/debug/deps/hello_world-be6f89c87ec85c85.d
@@ -0,0 +1,5 @@
+/home/pole/Scrivania/Programmazione_di_sistema/rust_programs/hello-world/target/debug/deps/hello_world-be6f89c87ec85c85.rmeta: src/main.rs
+
+/home/pole/Scrivania/Programmazione_di_sistema/rust_programs/hello-world/target/debug/deps/hello_world-be6f89c87ec85c85.d: src/main.rs
+
+src/main.rs:
