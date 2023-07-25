508948175498d0f90bab7ccf815b05017d10b4a6
pole9774 <pole97@live.it>
2023-07-25 16:32:26 +0200

    change

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,56 +1,16 @@
-fn make_box(a: i32) -> Box<(i32, i32)> {
-    let r = Box::new((a, 1));
-    return r;
-}
-
-fn add_numbers(x: i32, y: i32) -> i32 {
-    x + y
+fn test(v: &[&str]) {
+    for a in v {
+        println!("{}", a);
+    }
 }
 
 fn main() {
-    let v = 123;
-    let mut w = v;
-    let t = (7.5, true);
-    let mut c = (9.0, 1.3);
-    w = 42;
-    println!("{}", w);
-    println!("{}", v);
-    println!("{:?}", t);
-    c.0 = 5.42;
-    c.1 = 7.77;
-    println!("{:?}", c);
+    let mut ground = Vec::new();
+    let s = "aaaabbbbcccc".to_string();
 
-    let mut b = Box::new((1, 3));
-    println!("{:?}", b);
-    b.1 = 5;
-    println!("{:?}", b);
-
-    let m = make_box(51);
-    println!("{:?}", m);
-
-    let a = [1,2,3,4,5];
-    let a1 = [0;5];
-
-    println!("{:?} --- {:?}", a, a1);
-    println!("{}", a.len());
-    println!("{}", a[3]);
-
-    let s1 = &a[0..=2];
-    let s2 = &a[2..];
-    println!("{:?}", s1);
-    println!("{:?}", s2);
-
-    let mut vec = Vec::new();
-    vec.push(2);
-    vec.push(4);
-    println!("{:?}", vec);
-    println!("{}", vec[1]);
-
-    println!("{}", add_numbers(42, 11));
-    println!("Hello, world!!");
-
-    let names = ["Bob", "Frank", "Ferris"];
-    for (i, n) in names.iter().enumerate() {
-        println!("{} - {}", n, i);
+    for a in 0..3 {
+        let tmp = &s[a*4..(a+1)*4];
+        ground.push(tmp);
     }
+    test(&ground);
 }
