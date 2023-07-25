1300bbc900c906a39a3c88c3983fbf69e855866c
pole9774 <pole97@live.it>
2023-07-25 18:16:44 +0200

    change

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -4,6 +4,12 @@ fn test(v: &[&str]) {
     }
 }
 
+fn generator(prefix: &str) -> impl FnMut() -> String {
+    let mut i = 0;
+    let b = prefix.to_string();
+    move || {i+=1; format!("{}{}",b,i)}
+}
+
 fn main() {
     let mut ground = Vec::new();
     let s = "aaaabbbbcccc".to_string();
@@ -13,4 +19,53 @@ fn main() {
         ground.push(tmp);
     }
     test(&ground);
+
+
+    let s1 = "098 928 0982";
+    let sep = ' ';
+
+    let a1 = s1.chars().filter(|c| { *c == sep }).count();
+    println!("{}", a1);
+
+    let b1 = s1.chars().filter(|c| { *c == sep }).count();
+    println!("{}", b1);
+
+
+    let mut f = generator("id_");
+
+    for _ in 1..6 {
+        println!("{}",f());
+    }
+
+
+    let v = vec![5, 10, 14, 12, 15];
+    let names = vec!["Aria", "Prova", "Terra"];
+
+    /*
+    let mut is_ok = true;
+
+    for x in v {
+        if x < 6 || x > 15 {
+            is_ok = false;
+        }
+    }
+    */
+
+    // let is_ok = v.iter().all(|x| { *x >= 6 && *x <= 15 });
+
+    let is_ok = v.iter().any(|x| { *x >= 6 && *x <= 15 });
+
+    if is_ok {
+        println!("Ok");
+    } else {
+        eprintln!("Not ok");
+    }
+
+    // let v2: Vec<(&i32, &&str)> = v.iter().zip(names.iter()).collect();
+    let v2 = v.iter().zip(names.iter()).collect::<Vec<(_,_)>>();
+
+    for elem in v2 {
+        println!("{} - {}", elem.0, elem.1);
+    }
+
 }
