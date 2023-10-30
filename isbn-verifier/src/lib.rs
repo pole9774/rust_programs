fc8e8391dd6abdce0813dd4308add23519a6b9b2
pole9774 <pole97@live.it>
2023-10-30 13:26:57 +0100

    change

--- isbn-verifier/src/lib.rs
+++ isbn-verifier/src/lib.rs
@@ -1,4 +1,6 @@
-/// Determines whether the supplied string is a valid ISBN number
+// Determines whether the supplied string is a valid ISBN number
+
+/*
 pub fn is_valid_isbn(isbn: &str) -> bool {
     let mut t = 0;
     let mut w = 10;
@@ -13,7 +15,27 @@ pub fn is_valid_isbn(isbn: &str) -> bool {
         w -= 1;
     }
     t % 11 == 0 && w == 0
-}
+} 
+*/
+
+pub fn is_valid_isbn(isbn: &str) -> bool {
+    let mut t = 0;
+    let mut w = 10;
+    let s = isbn.replace("-", "");
+    if s.chars().count() != 10 {
+        return false;
+    }
+    for ch in isbn.chars() {
+        match ch {
+            '-' => continue,
+            'X' if w==1 => t+=10,
+            '0'..='9' => t += ch.to_digit(10).unwrap() * w,
+            _ => return false
+        }
+        w -= 1;
+    }
+    t % 11 == 0
+} 
 
 // cargo test
 // cargo test -- --ignored
