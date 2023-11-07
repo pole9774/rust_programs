5580d9283dc22ce6e730d34273ad1aa6a6fa991c
pole9774 <pole97@live.it>
2023-11-07 17:06:50 +0100

    change

--- minesweeper/src/lib.rs
+++ minesweeper/src/lib.rs
@@ -1,68 +1,15 @@
-pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    let r = minefield.len();
-
-    if r == 0 {
-        return vec![];
-    }
-
-    let c = minefield[0].len();
-
-    let mut res = vec![];
-
-    if c == 0 {
-        res.push("".to_string());
-        return res;
-    }
-
-    for(i, row) in minefield.iter().enumerate() {
-        let mut new_row = String::new();
-        for(j, elem) in row.chars().enumerate() {
-            let num_ad = count_ad(minefield, i, j);
-            let tmp = if elem == ' ' && num_ad > 0 {
-                num_ad.to_string().chars().nth(0).unwrap()
-            } else {
-                elem
-            };
-            new_row.push(tmp);
-        }
-        res.push(new_row);
-    }
-    res
-}
-
-/*
 fn count_ad(minefield: &[&str], row: usize, col: usize) -> u32 {
     let r = minefield.len();
     let c = minefield[0].len();
-
-    let mut cnt = 0;
-
-    for i in (row.saturating_sub(1))..=(row + 1).min(r - 1) {
-        for j in (col.saturating_sub(1))..=(col + 1).min(c - 1) {
-            if i != row || j != col {
-                if minefield[i].chars().nth(j).unwrap_or(' ') == '*' {
-                    cnt += 1;
-                }
-            }
-        }
-    }
-    cnt
-}
-*/
-
-fn count_ad(minefield: &[&str], row: usize, col: usize) -> u32 {
-    let r = minefield.len();
-    let c = minefield[0].len();
+    let mut res = 0;
 
     let mut start_row = 0;
     let mut start_col = 0;
 
-    let mut cnt = 0;
-
-    if row != 0 {
+    if row > 0 {
         start_row = row - 1;
     }
-    if col != 0 {
+    if col > 0 {
         start_col = col - 1;
     }
 
@@ -70,10 +17,41 @@ fn count_ad(minefield: &[&str], row: usize, col: usize) -> u32 {
         for j in start_col ..= (col+1).min(c-1) {
             if i != row || j != col {
                 if minefield[i].chars().nth(j).unwrap() == '*' {
-                    cnt += 1;
+                    res += 1;
                 }
             }
         }
     }
-    cnt
+    res
+}
+
+pub fn annotate(minefield: &[&str]) -> Vec<String> {
+    let r = minefield.len();
+    
+    if r == 0 {
+        return Vec::new();
+    }
+
+    let c = minefield[0].len();
+    let mut res = Vec::new();
+
+    if c == 0 {
+        res.push("".to_string());
+        return res;
+    }
+
+    for (i, row) in minefield.iter().enumerate() {
+        let mut new_row = String::new();
+        for (j, ch) in row.chars().enumerate() {
+            let n = count_ad(minefield, i, j);
+            let tmp = if ch == ' ' && n > 0 {
+                n.to_string().chars().nth(0).unwrap()
+            } else {
+                ch
+            };
+            new_row.push(tmp);
+        }
+        res.push(new_row);
+    }
+    res
 }
